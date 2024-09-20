d79b20502ef61557511a261231bc849bf7ffd907
Admin <aftabmaner3131@gmail.com>
2024-09-20 18:04:44 +0530

    first comit

--- lib/View/home.dart
+++ lib/View/home.dart
@@ -13,7 +13,7 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Home'),
+        title: Text('Home Page'),
       ),
       body: Obx(() => Column(
         children: [
